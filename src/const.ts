d99b2fb1220508e5839a3db0e8684bb6acf201bb
wedding260314 <jainy0v0@gmail.com>
2025-12-04 00:46:05 +0900

    Update const.ts

--- src/const.ts
+++ src/const.ts
@@ -30,7 +30,7 @@ export const WEDDING_HALL_POSITION = [126.9594982, 37.4657134]
 // 네이버 지도의 웨딩홀 장소 ID
 // 네이버 지도 웹페이지에서 웨딩홀 검색 후 URL에서 확인 가능.
 // 예: https://map.naver.com/p/entry/place/13321741 -> 13321741
-export const NMAP_PLACE_ID = 13321741
+export const NMAP_PLACE_ID = 19083380
 
 // 카카오 지도의 웨딩홀 장소 ID
 // 카카오 지도 웹페이지에서 웨딩홀 검색 후 해당 장소에서 상세보기 클릭 시 URL에서 확인 가능.
