5a0803a0a059c0e12221985c1e01bc53c074e22b
wedding260314 <jainy0v0@gmail.com>
2025-12-06 10:42:31 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,36 +12,44 @@ import { STATIC_ONLY } from "./env"
 function App() {
   return (
         <div className="background">
-      <div className="card-view">
-        <LazyDiv className="card-group cover-full">
+      <div className="card-group cover-full">
+        <LazyDiv className="card">
           {/* 표지 */}
           <Cover />
+             </LazyDiv>
 
+          <LazyDiv className="card">
           {/* 모시는 글 */}
           <Invitation />
         </LazyDiv>
+      </div>
 
-        <LazyDiv className="card-group">
+      <div className="card-group">
+        <LazyDiv className="card">
           {/* 결혼식 날짜 (달력) */}
           <Calendar />
+             </LazyDiv>
 
+          <LazyDiv className="card"> 
           {/* 겔러리 */}
           <Gallery />
         </LazyDiv>
+       </div>
 
-        <LazyDiv className="card-group">
+       <div className="card-group">
+        <LazyDiv className="card">
           {/* 오시는길 */}
           <Location />
         </LazyDiv>
 
-        <LazyDiv className="card-group">
+        <LazyDiv className="card">
           {/* 마음 전하기 */}
           <Information />
         </LazyDiv>
+      </div>
 
         <ShareButton />
       </div>
-    </div>
   )
 }
 
