c48fd5b1a28afd33d5a4fe3c224a71ce2e5163d1
wedding260314 <jainy0v0@gmail.com>
2025-12-08 22:53:56 +0900

    Update index.tsx

--- src/component/location/index.tsx
+++ src/component/location/index.tsx
@@ -18,59 +18,26 @@ export const Location = () => {
       </LazyDiv>
       <LazyDiv className="card location">
         <div className="location-info">
-          <div className="transportation-icon-wrapper">
-            <BusIcon className="transportation-icon" />
-          </div>
-          <div className="heading">대중교통</div>
-          <div />
+      
           <div className="content">
-            * 지하철 이용시
-            <br />
-            지하철 2호선 <b>낙성대역 3번출구</b> 나와서
-            <br />
-            → 첫번째 골목 끼고 좌회전
-            <br />→ 마을버스 <b>관악 02번</b> 승차
-            <br />→ <b>서울대후문·연구공원 정류장</b> 하차
-            <br />
-            → 길 건너 간판 참고해 도보로 100m 이동
-            <br />
-            검은색 피라미드 유리 건물입니다.
+            <div className="heading">대중교통</div>
+             회관으로 들어오는 길은 인도가 없어서
+             <br />도보로 오시기에는 매우 위험합니다.
+            <br /> <b>평택역</b> 또는 <b>공도 시외버스 터미널</b>에서 하차하셔서
+            <br /> <b>택시</b>를 이용해 방문해주시기를 부탁드립니다.
+             <br /> 
           </div>
-          <div />
-          <div className="content">
-            * 버스 이용 시
-            <br />
-            - 간선(파랑): 461, 641
-            <br />
-            - 지선(초록): 5413, 5524, 5528
-            <br />
-            반드시 <b>낙성대입구</b> 하차
-            <br />→ 마을버스 <b>관악 02번</b> 이용
-            <br />
-            이하 위와 동일합니다.
-          </div>
-        </div>
+         
         <div className="location-info">
-          <div className="transportation-icon-wrapper">
-            <CarIcon className="transportation-icon" />
           </div>
           <div className="heading">자가용</div>
+           <b>경기도 안성시 공도읍 신두만곡로 101</b>
+           <br />주소를 정확히 검색하시어 근처 다른 회관으로
+           <br />도착하지 않도록 주의해 주시기 바랍니다.
           <div />
           <div className="content">
-            네이버 지도, 카카오 네비, 티맵 등 이용
-            <br />
-            <b>서울대학교 연구공원 웨딩홀</b> 검색
-            <br />
-            - 주차 요금은 무료입니다.
-            <br />
-            (주차장 이용 시 웨딩홀과 바로 연결)
-          </div>
-          <div />
-          <div className="content">
-            <b>
-              ※ 서울대학교 정, 후문을 통과할 경우 통행료가 발생하므로
-              유의바랍니다. 낙성대 방향으로 이용해주세요.
-            </b>
+          <br /> 주차공간이 협소한 관계로 가능하신 경우
+           <br />일행분들과 카풀해주신다면 감사드리겠습니다.
           </div>
         </div>
       </LazyDiv>
