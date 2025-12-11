bd06fee1f05ce22d0886f3a29ab20a973a7b2eec
wedding260314 <jainy0v0@gmail.com>
2025-12-11 22:43:55 +0900

    Update index.tsx

--- src/component/information/index.tsx
+++ src/component/information/index.tsx
@@ -7,11 +7,17 @@ export const Information = () => {
       <p className="title-kr">안내사항</p>
 
       <div className="notice-card">
-        언제나 저희 두 사람에게 사랑과 관심을 나타내주셔서
+        언제나 저희 두 사람에게
+        <br />
+        사랑과 관심을 나타내주셔서
         <br />
         정말 감사드립니다.
+        <br /><br />
+        저희는 축의금 대신 
         <br />
-        저희는 축의금 대신 축하해주시는 마음만 소중히 받겠습니다.
+        축하해주시는 마음만 
+        <br />
+        소중히 받겠습니다.
       </div>
     </LazyDiv>
   )
