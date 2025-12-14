89931e3639ace2949a803504beff02b781b0b44f
wedding260314 <jainy0v0@gmail.com>
2025-12-15 00:24:15 +0900

    Update index.tsx

--- src/component/information/index.tsx
+++ src/component/information/index.tsx
@@ -3,21 +3,11 @@ import { LazyDiv } from "../lazyDiv"
 export const Information = () => {
   return (
     <LazyDiv className="card notice">
-      <h2 className="english">Notice</h2>
-      <p className="title-kr">안내사항</p>
+     // <h2 className="english">Notice</h2>
+      //<p className="title-kr">안내사항</p>
 
       <div className="notice-card">
-        언제나 저희 두 사람에게
-        <br />
-        사랑과 관심을 나타내주셔서
-        <br />
-        정말 감사드립니다.
-        <br /><br />
-        저희는 축의금 대신 
-        <br />
-        축하해주시는 마음만 
-        <br />
-        소중히 받겠습니다.
+         
       </div>
     </LazyDiv>
   )
