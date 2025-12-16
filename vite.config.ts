654287349b6a1de13d093a20005301a57d70de7d
wedding260314 <jainy0v0@gmail.com>
2025-12-16 23:37:41 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import svgr from "vite-plugin-svgr"
 import fs from "fs"
-
-import pkg from "./package.json"
 import { createHtmlPlugin } from "vite-plugin-html"
+
 import {
   GROOM_FULLNAME,
   BRIDE_FULLNAME,
@@ -15,19 +14,9 @@ import {
 
 const distFolder = "build"
 
-let base = "/"
-
-try {
-  const url = new URL(pkg.homepage)
-  base = url.pathname
-} catch (e) {
-  base = pkg.homepage || "/"
-}
-
-
-// https://vite.dev/config/
 export default defineConfig({
- 
+  base: "/wedding-invitation/",
+
   plugins: [
     react(),
     svgr(),
@@ -51,7 +40,7 @@ export default defineConfig({
       },
     },
   ],
+
   server: { port: 3000 },
   build: { outDir: distFolder },
-   base: "/wedding-invitation/",
 })
