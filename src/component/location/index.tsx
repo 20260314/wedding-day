64d80982cd081ec3d5ad3e1362e55fc81b3db48e
wedding260314 <jainy0v0@gmail.com>
2026-01-20 16:43:46 +0900

    Update index.tsx

--- src/component/location/index.tsx
+++ src/component/location/index.tsx
@@ -39,11 +39,9 @@ export const Location = () => {
         
         {/*온라인*/}
         <div className="location-block">
-          <div className="heading">줌 ZOOM</div>
+          <div className="heading">줌 ZOOM 주소</div>
           <div class="line"></div>
           <div className="content">
-            줌 (ZOOM) 으로도 결혼식이 생중계됩니다.
-            <br />
             회의 ID: 823 6639 8235
             <br />
             암호: 0314
